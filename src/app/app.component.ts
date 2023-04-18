f06cbce7112baba964d09ff10883fd864b4276c4
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-18 12:07:19 +0530

    secondary outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { slideInAnimation} from './app.animation'
 
 import { AuthService } from './user/auth.service';
-import { Router } from '@angular/router';
+import { Router,Event,NavigationStart,NavigationEnd,NavigationError,NavigationCancel, RouterEvent } from '@angular/router';
+import { MessageService } from './messages/message.service';
 
 @Component({
   selector: 'pm-root',
@@ -12,9 +13,13 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
+  loading =true;
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
+  }
+  get isMessageDisplayed():boolean{
+    return this.messageService.isDisplayed;
   }
 
   get userName(): string {
@@ -24,7 +29,28 @@ export class AppComponent {
     return '';
   }
 
-  constructor(private authService: AuthService,private router:Router) { }
+  constructor(private authService: AuthService,private router:Router,private messageService: MessageService) {
+    router.events.subscribe((routerEvent:Event)=>{
+      this.checkRouterEvent(routerEvent);
+    });
+   }
+  checkRouterEvent(routerEvent: Event) {
+    if(routerEvent instanceof NavigationStart){
+      this.loading=true;
+    }
+    if(routerEvent instanceof NavigationEnd||
+      routerEvent instanceof NavigationCancel||
+      routerEvent instanceof NavigationError){
+        this.loading=false;
+      }
+  }
+  displayMessages():void {
+    this.router.navigate([{outlets:{popup:['messages']}}]);
+    this.messageService.isDisplayed=true;
+  }
+  hideMessages():void{
+    this.messageService.isDisplayed=false
+  }
 
   logOut(): void {
     this.authService.logout();
