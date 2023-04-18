950d570faa700974c4066c75881225b2cc2176ac
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-04-18 15:52:06 +0530

    custom preLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule ,PreloadAllModules} from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy} from './selective-strategy.service'
 
 
 @NgModule({
@@ -13,11 +14,12 @@ import { AuthGuard } from './user/auth.guard';
             {path: 'products',
             // canLoad:[AuthGuard],
             canActivate:[AuthGuard],
+            data:{preload:false},
                 loadChildren:()=>
                     import('./products/product.module').then(m=>m.ProductModule)},
             {path:'',redirectTo:'welcome',pathMatch:'full'},
             {path:'**',component:PageNotFoundComponent}
-        ],{preloadingStrategy:PreloadAllModules})
+        ],{preloadingStrategy: SelectiveStrategy})
     ],
     exports:[RouterModule]
 })
